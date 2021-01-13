8944d9ccee191eea09ac6353f4882d922fd11361
Vladislav <mitisit89@gmail.com>
2021-01-13 14:27:22 +0300

    initital server

---
+++ Server/config.py
@@ -0,0 +1,12 @@
+import os
+
+
+class Configuration(object):
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    DEBUG = True
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    JSON_SORT_KEYS = False
+    CORS_METHODS = ['GET', 'POST', 'OPTION', 'DELETE']
